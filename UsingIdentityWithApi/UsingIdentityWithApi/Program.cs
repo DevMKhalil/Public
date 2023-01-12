489049194b53d63eb84d41d55a925bf89cd400f7
DevMKhalil <dev.mohammedkhalil@gmail.com>
2023-01-13 01:31:09 +0200

    add services

--- UsingIdentityWithApi/UsingIdentityWithApi/Program.cs
+++ UsingIdentityWithApi/UsingIdentityWithApi/Program.cs
@@ -8,81 +8,26 @@ using System.Reflection;
 using UsingIdentityWithApi.Application;
 using UsingIdentityWithApi.Data;
 using UsingIdentityWithApi.Logic;
+using UsingIdentityWithApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-ILoggerFactory ConsolLoggerFactory = LoggerFactory.Create(builder =>
-{
-    builder.AddConsole();
-    builder.AddFilter(DbLoggerCategory.Database.Transaction.Name, LogLevel.Debug);
-    builder.AddFilter(DbLoggerCategory.Database.Connection.Name, LogLevel.Information);
-});
 
-builder.Services.AddDbContext<UsingIdentityWithApiContext>(options =>
-{
-    options
-    .UseLoggerFactory(ConsolLoggerFactory)
-    .EnableSensitiveDataLogging()
-    .UseSqlServer(builder.Configuration.GetValue<string>("DefaultConnection"));
+// Add Context Services
+builder.Services.AddContext(builder.Configuration.GetValue<string>("DefaultConnection"));
 
-});
+//builder.Services.AddAuthentication("cookies").AddCookie("cookies", options => options.LoginPath = "/api/Users/Login");
 
-//builder.Services.AddDefaultIdentity<ApiUser>(options => { })
-//        .AddEntityFrameworkStores<UsingIdentityWithApiContext>();
-
-//builder.Services.AddAuthentication(o =>
-//{
-//    o.DefaultScheme = IdentityConstants.ApplicationScheme;
-//    o.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-//})
-//.AddIdentityCookies(o => { });
-
-//builder.Services.AddIdentityCore<ApiUser>(o =>
-//{
-//    o.Stores.MaxLengthForKeys = 128;
-//    o.SignIn.RequireConfirmedAccount = true;
-//})
-//.AddDefaultTokenProviders();
-builder.Services.AddAuthentication("cookies").AddCookie("cookies", options => options.LoginPath = "/api/Users/Login");
-
-builder.Services.AddIdentityCore<ApiUser>(options => {
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-});
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => {
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-})
-    .AddEntityFrameworkStores<UsingIdentityWithApiContext>()
-    .AddDefaultTokenProviders();
-
-
-//builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-//{
-//})
-//            .AddRoles<IdentityRole>()
-//            .AddEntityFrameworkStores<UsingIdentityWithApiContext>()
-//            .AddDefaultTokenProviders()
-//            .AddSignInManager();
-
-
-builder.Services.AddScoped<IUserStore<ApiUser>, ApiUserStore>();
-builder.Services.AddScoped<ApiUserManager>();
-//builder.Services.AddScoped<IUserStore<ApiUser>,UserOnlyStore<ApiUser, UsingIdentityWithApiContext>>();
-//builder.Services.AddScoped<IUserStore<ApiUser>, CustomIdentityUserStore>();
+// Add Identity Services
+builder.Services.AddIdentityForIdentityUser();
+builder.Services.AddIdentityForAspNetIdentityUser();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
-builder.Services.AddTransient<IUsingIdentityWithApiContext, UsingIdentityWithApiContext>();
 builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 var app = builder.Build();
---
+++ UsingIdentityWithApi/UsingIdentityWithApi/ServicesExtention.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UsingIdentityWithApi.Application;
+using UsingIdentityWithApi.Data;
+using UsingIdentityWithApi.Logic;
+using static CSharpFunctionalExtensions.Result;
+
+namespace UsingIdentityWithApi
+{
+    public static class ServicesExtention
+    {
+        public static void AddIdentityForIdentityUser(this IServiceCollection services)
+        {
+            services.AddIdentityCore<ApiUser>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            });
+
+            services.AddScoped<IUserStore<ApiUser>, ApiUserStore>();
+            services.AddScoped<ApiUserManager>();
+        }
+
+        public static void AddIdentityForAspNetIdentityUser(this IServiceCollection services)
+        {
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
+            .AddEntityFrameworkStores<UsingIdentityWithApiContext>()
+            .AddDefaultTokenProviders();
+        }
+
+        public static void AddContext(this IServiceCollection services, string connectionString)
+        {
+            ILoggerFactory ConsolLoggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+                builder.AddFilter(DbLoggerCategory.Database.Transaction.Name, LogLevel.Debug);
+                builder.AddFilter(DbLoggerCategory.Database.Connection.Name, LogLevel.Information);
+            });
+
+            services.AddDbContext<UsingIdentityWithApiContext>(options =>
+            {
+                options
+                .UseLoggerFactory(ConsolLoggerFactory)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(connectionString);
+
+            });
+
+            services.AddTransient<IUsingIdentityWithApiContext, UsingIdentityWithApiContext>();
+        }
+    }
+}
