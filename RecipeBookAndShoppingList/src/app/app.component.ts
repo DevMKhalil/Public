e6e86da320ce66bd19b5b388a458d0414a7977f9
DevMKhalil <dev.mohammedkhalil@gmail.com>
2023-06-03 23:51:45 +0200

    WIP

--- RecipeBookAndShoppingList/src/app/app.component.ts
+++ RecipeBookAndShoppingList/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.authService.autoLogin();
+  }
+
   title = 'RecipeBookAndShoppingList';
+
+  constructor(private authService: AuthService) { }
 }
--- RecipeBookAndShoppingList/src/app/auth/auth.service.ts
+++ RecipeBookAndShoppingList/src/app/auth/auth.service.ts
@@ -39,9 +39,31 @@ export class AuthService {
     );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData') ?? '');
+
+    if (!userData)
+      return;
+
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token)
+      this.user.next(loadedUser);
+  }
+
   LogOut() {
     this.user.next(null);
     this.router.navigate(['./auth']);
+    localStorage.removeItem('userData');
+  }
+
+  autoLogOut() {
+
   }
 
   LogIn(email: string, password: string) {
@@ -65,6 +87,7 @@ export class AuthService {
     const tokenExpirationDate: Date = new Date(new Date().getTime() + (expiresIn * 1000));
     const user = new User(email, userId, token, tokenExpirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private HandelError(errorRes: HttpErrorResponse) {
